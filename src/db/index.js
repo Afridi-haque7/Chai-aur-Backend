6dbc85e553eee8e9aa17914a5c4385f6e60e8c53
Afridi <afridi.hq97@gmail.com>
2024-04-08 16:35:12 +0530

    mongodb connection

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`MongoDB connected! `);
+    } catch (error) {
+        console.log("MongoDB Connection Error", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
