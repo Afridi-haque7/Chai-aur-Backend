aa3d55dfd62d1bf8fa07951222e4b956490c5fda
Afridi <afridi.hq97@gmail.com>
2024-04-08 17:04:07 +0530

    new commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
+
 
 dotenv.config();
-connectDB();
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at PORT ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Failed!!", err);
+  });
